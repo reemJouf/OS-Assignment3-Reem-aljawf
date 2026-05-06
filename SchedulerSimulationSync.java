4b6bf93ce9f584fdf4677718b622dcb7ee9797b9
Reem <445052182@std.psau.edu.sa>
2026-05-07 00:34:20 +0300

    protecting excutionLock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -85,7 +85,13 @@ class SharedResources {
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+             lock.lock();
+        try{
+            executionLog.add(message);
+        }finally{
+            lock.unlock();
+        }
+       
     }
 }
 
